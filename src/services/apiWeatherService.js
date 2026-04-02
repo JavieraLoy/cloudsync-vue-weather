22126aa88fcaf826715ef073ad7be781dd7faf28
javiloy <246564344+JavieraLoy@users.noreply.github.com>
2026-04-02 16:11:35 -0300

    se retira api key

--- src/services/apiWeatherService.js
+++ src/services/apiWeatherService.js
@@ -1,4 +1,4 @@
-const API_KEY = '29a7f1e89596eb44b2c21cd07df4fdcc';
+const API_KEY = '';
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 export async function obtenerClimaActual(ciudad) {
